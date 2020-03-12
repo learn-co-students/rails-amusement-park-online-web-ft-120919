c6bbb82a4e804feaeaa76cea29340bbcbfc6c20f
allison_johnson <allison.cuttler@gmail.com>
2020-03-11 21:56:16 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,20 +17,18 @@ class UsersController < ApplicationController
   end #create
 
   def show
-    #binding.pry 
-    #if params[:controller] == "attractions" #got here via post request
-    if request.post?  
+    if request.post? #got here via post request  
       @ride = Ride.create(user_id: session[:user_id], attraction_id: params[:id] )
       flash[:message] = @ride.take_ride
       @user = User.find_by(id: session[:user_id])
 
       if flash[:message] == true
         flash[:message] = "Thanks for riding the #{Attraction.find_by(id: params[:id]).name}!"
-        #binding.pry 
         redirect_to user_path(@user)
       else
         redirect_to user_path(@user)
       end
+
     else #got here via get request
       if session[:user_id]
         set_user
--- notes.md
+++ notes.md
@@ -1,1 +1,7 @@
-  <%= text_field_tag :name %><br><br>
+
+
+<%= form_tag(edit_attraction_path, method: 'get') do %>
+     <%= submit_tag("Edit Attraction")%>
+<% end %>
+
+<%= link_to "Edit Attraction", edit_attraction_path(@attraction) %>
