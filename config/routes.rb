9e0205fd0b10a18c8231850b78eb0ab2fe7a547f
ricardo39985 <ricardo39985@gmail.com>
2020-03-15 11:57:18 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'sessions/new'
+  get 'sessions/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "home#index"
+  get "/users/new", to: 'sessions#new'
 end
